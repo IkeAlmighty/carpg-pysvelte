319e387af7c5c2a2b4acc34039d963575163326e
IkeAlmighty <IsaacYates7@gmail.com>
2021-03-23 13:23:57 -0500

    added flaskapp routes for serving static svelte in production

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 from flask_cors import CORS
 import importlib
 import json
@@ -6,12 +6,21 @@ import json
 app = Flask(__name__)
 CORS(app)
 
-
+# api path setup:
 @app.route('/api/<resource>')
 def call_api(resource):
     res = importlib.import_module("api.{}".format(resource)).get(request)
     return json.dumps(res)
 
+# path for main svelte page:
+@app.route('/')
+def base():
+    return send_from_directory('svelte-client/public', 'index.html')
+
+# path for all frontend static files
+@app.route('/<path:path>')
+def home(path):
+    return send_from_directory('svelte-client/public', path)
 
 if __name__ == "__main__":
     app.run()
